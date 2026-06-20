a36ba6950cfac4bbf910dc8cce77ded6431397ea
ialiejaz <ialiejaz33@gmail.com>
2026-06-20 14:03:51 +0000

    data types

---
+++ basics/data_types.js
@@ -0,0 +1,28 @@
+"use strict"; //to use modern syntax for JS code
+// visit https://tc39.es/ecma262/
+// visit https://developer.mozilla.org/ for documentation
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.table([[typeof name],[typeof age],[typeof isLoggedIn],[typeof state],[typeof null]])
