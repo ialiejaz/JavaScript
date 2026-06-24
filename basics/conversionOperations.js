0e4de3517cfb4df5b5daa29504d851e43547af61
ialiejaz <ialiejaz33@gmail.com>
2026-06-24 15:20:56 +0000

    conversionOperations

---
+++ basics/conversionOperations.js
@@ -0,0 +1,34 @@
+let points = 33
+let value = true
+let value2 = false
+let name = ("value")
+let address = Boolean("CTN")
+
+//console.log(typeof (points));
+
+let runs = Number(points)
+
+//console.table([typeof points,runs,value, value2,typeof value ,typeof value2,typeof name, typeof address,typeof runs])
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let loggedIn = 0
+let loggedOut = 1
+
+let inBoolean = Boolean(loggedIn)
+let outBoolean = Boolean(loggedOut)
+
+console.table([inBoolean,outBoolean])
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let chak = 33
+
+let pind = Number(chak)
+
+console.table([pind,typeof pind]);
