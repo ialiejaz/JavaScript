1e468c210e4c8f5a4c147caf3a7e8e342b21f4dc
ialiejaz <ialiejaz33@gmail.com>
2026-06-26 14:17:55 +0000

    summary

---
+++ basics/summary.js
@@ -0,0 +1,18 @@
+let name = "ali"
+
+let name2 = name
+name2 = "umer"
+
+// console.log(name)
+// console.log(name2);
+
+let user1 = {
+    email : "ali@gmail.com",
+    id : 123
+}
+
+let user2 = user1
+user2.email="umer@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
