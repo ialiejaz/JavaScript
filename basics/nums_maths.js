78388b01bd4ad5dfa179c503d3fcdb1f3d4d528a
ialiejaz <ialiejaz33@gmail.com>
2026-07-27 03:48:21 -0700

    nums and maths

---
+++ basics/nums_maths.js
@@ -0,0 +1,40 @@
+const score = 100
+console.log(score);
+
+const totalScore = new Number(150)
+const displayScore = totalScore.toString()
+
+console.log(totalScore)
+console.log(typeof(displayScore))
+
+const otherNumber = 123.876
+console.log(otherNumber.toPrecision(4));
+
+const balance = 100000000;
+console.log(balance.toLocaleString('en-PK'))
+
+
+// *********************************** MATHS ****************************************
+
+const length = 126.345
+console.table({
+    Input: length,
+    "Math.round" : Math.round(length),
+    "Math.ceil" : Math.ceil(length),
+    "Math.floor" : Math.floor(length),
+    "Math.sqrt" : Math.sqrt(length),
+    "Math.abs" : Math.abs(length)    
+})
+
+// ************************* Random method() ******************************
+
+console.log(Math.random())
+
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
+
