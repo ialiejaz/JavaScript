ca29d3479258d6985f0e36f3b6c127821e627b33
ialiejaz <ialiejaz33@gmail.com>
2026-08-01 06:56:37 -0700

    time and date

--- basics/dates.js
+++ basics/dates.js
@@ -33,11 +33,36 @@
     
 // })
 
-let dateSyntax = new Date(2025,5,10) // (YYYY/MM/DD)
-let dateSyntax2 = new Date("2025-5-10") // (DD/MM/YYYY)
+// let dateSyntax = new Date(2025,5,10) // (YYYY/MM/DD)
+// let dateSyntax2 = new Date("2025-5-10") // (DD/MM/YYYY)
 
-console.table({
-   "YYYY/MM/DD"  : dateSyntax.toString(),
-    "DD/MM/YYYY" : dateSyntax2.toDateString()
-})
-//console.log(dateSyntax2);
+// console.table({
+//    "YYYY/MM/DD"  : dateSyntax.toString(),
+//     "DD/MM/YYYY" : dateSyntax2.toDateString()
+// })
+
+
+let myTime = Date.now()
+let oldTime = new Date("10-10-2010")
+
+// console.log(myTime);
+// console.log(oldTime.getTime());
+// console.log(oldTime.toLocaleString());
+
+//Converting milliseconds to seconds
+
+console.log(myTime)
+console.log(myTime/1000)
+console.log(Math.floor(myTime/1000));
+
+let thisDate = new Date()
+
+console.log(thisDate);
+console.log(thisDate.getDay());
+console.log(thisDate.getMonth()+1);
+
+console.log(`This is the ${thisDate.getDay()}th day of the week and ${thisDate.getMonth()+1}th month of the year.`)
+
+console.log(thisDate.toLocaleString('dafault',{
+    weekday: 'long'
+}));
